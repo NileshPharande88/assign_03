0629ca98bafe29f7746bbb76ef3341358eb044b7
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-03-03 12:08:01 +0530

    Changed code for query String checking.

--- httpServer.js
+++ httpServer.js
@@ -2,6 +2,7 @@ try {
     var http = require("http");
     var url = require('url');
     var fs = require('fs');
+    var qs = require('querystring');
     //Checks the  required modules are available or not.
     if ((http === undefined)) throw new Error( " Can't access http module" );
     if ((url === undefined)) throw new Error( " Can't access url module" );
@@ -11,24 +12,31 @@ try {
     var server = http.createServer ( function(req,res) {
         //Checks and allows only the GET requests from the requests coming from client.
         if (req.method === 'GET'){
-            var parts = req.url.split('/');
-            if( parts[1] !== "favicon.ico") {
+            var urlParts = req.url.split('/')[1];
+            if( urlParts !== "favicon.ico") {
+                var qParam = qs.parse( req.url.split('?')[1] );
+                var imageName = qParam.image;//.toLowerCase();   //Converte folder name to lower case.
                 //Checks the existence of the images of given name from the client request in Image folder.
-                fs.exists ( "Images/" + parts[1], function (exists) {
-                    console.log("test: ", exists);
+                fs.exists ( "Images/" + imageName, function (exists) {
+                    console.log("test: ", exists, imageName );
                     if ( exists ) {
                         //Read synchronously the images from Image folder.
-                        var img = fs.readFileSync( "Images/" + parts[1] );
+                        var img = fs.readFileSync( "./Images/" + imageName );
                         res.writeHead(200, {'Content-Type': 'image/gif' });
                         res.end(img, 'binary');
                     } else {
                         //shows error message when image not found.
                         res.writeHead(400, {"Content-Type": "text/html"});
-                        res.write("<h1>File not found " + parts[1] + "</h1>");
+                        res.write("<h1>File not found " + imageName + "</h1>");
                         res.end();
                     }
                 });
+            } else {
+                res.end(); //Avoid un necessory execution of code.
             }
+        } else {
+            console.log("Server closed: Request is other than GET request."); 
+            res.end("Server closed: Request is other than GET request.");
         }
     });
     server.listen(1337, "127.0.0.1", function(){
