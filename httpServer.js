d1f99bc3fff1951ad3142536ba2b120cf1e6eced
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-03-23 14:39:35 +0530

    if else ladder was removed.

--- httpServer.js
+++ httpServer.js
@@ -4,45 +4,43 @@ try {
     var fs = require('fs');
     var qs = require('querystring');
     //Checks the  required modules are available or not.
-    if ((http === undefined)) throw new Error( " Can't access http module" );
-    if ((url === undefined)) throw new Error( " Can't access url module" );
-    if ((fs === undefined)) throw new Error( " Can't access fs module" );
+    if (fs === undefined) {
+        throw new Error(" Can't access fs module.");
+    }
+    if (http === undefined) {
+        throw new Error(" Can't access http module.");
+    }
+    if (url === undefined) {
+        throw new Error(" Can't access url module.");
+    }
 
     //Function which get called everytime when server receives therequest from client.
-    var server = http.createServer ( function(req,res) {
-        //Checks and allows only the GET requests from the requests coming from client.
-        if (req.method === 'GET'){
-            var urlParts = req.url.split('/')[1];
-            if( urlParts !== "favicon.ico") {
-                var qParam = qs.parse( req.url.split('?')[1] );
-                var imageName = qParam.image;//.toLowerCase();   //Converte folder name to lower case.
-                //Checks the existence of the images of given name from the client request in Image folder.
-                fs.exists ( "Images/" + imageName, function (exists) {
-                    console.log("test: ", exists, imageName );
-                    if ( exists ) {
-                        //Read synchronously the images from Image folder.
-                        var img = fs.readFileSync( "./Images/" + imageName );
-                        res.writeHead(200, {'Content-Type': 'image/gif' });
-                        res.end(img, 'binary');
-                    } else {
-                        //shows error message when image not found.
-                        res.writeHead(400, {"Content-Type": "text/html"});
-                        res.write("<h1>File not found " + imageName + "</h1>");
-                        res.end();
-                    }
-                });
-            } else {
-                res.end(); //Avoid un necessory execution of code.
-            }
-        } else {
-            console.log("Server closed: Request is other than GET request."); 
+    var server = http.createServer ( function (req, res) {
+        if (req.method !== 'GET') {
             res.end("Server closed: Request is other than GET request.");
+            throw new Error(" Server closed: Request is other than GET request.");
+        }  //Proccessvthe GET request further.
+        if( req.url.split('/')[1] === "favicon.ico") {
+            res.end(); //Avoid un necessory execution of code.
+        } else {
+            //Findout imagename from the query string parameter.
+            var imageName = qs.parse( req.url.split('?')[1] ).image;//.toLowerCase();//Converte folder name to lower case.
+            fs.exists( "Images/" + imageName, function (exists) {
+                if ( !exists ) {  //shows error message when image not found.
+                    res.writeHead(400, {"Content-Type": "text/html"});
+                    res.end("<h1>Image not found: " + imageName + "</h1>");
+                    throw new Error(" Image not found with the name given by client.");
+                }
+                //Read synchronously the images from Image folder.
+                var img = fs.readFileSync( "./Images/" + imageName );
+                res.writeHead(200, {'Content-Type': 'image/gif'});
+                res.end(img, 'binary');
+            });
         }
     });
     server.listen(1337, "127.0.0.1", function(){
         console.log( "Listening on- 127.0.0.1:1337" );
     });
-    
-} catch ( errorMessage) {
-    console.log( "Error: ", errorMessage);
+} catch (err) {
+    console.log(err);
 }
